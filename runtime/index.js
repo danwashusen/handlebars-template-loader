5258f5d633d64f4c1bc94917f6214b788e8c1daf
dan <dan@reactive.org>
2016-07-16 11:33:29 +1000

    Switched back the full handlebars runtime to support compile. Changed the compilers 'nameLookup' method to support backbone models.

--- runtime/index.js
+++ runtime/index.js
@@ -1,1 +1,17 @@
-module.exports = require('handlebars/runtime');
+// allow compile calls at runtime
+var Handlebars = require('handlebars'); 
+
+// support backbone model property lookups
+Handlebars.JavaScriptCompiler.prototype.nameLookup = function(parent, name, type) {
+    var result = '(' + parent + ' instanceof Backbone.Model ? ' + parent + '.get(\'' + name + '\') : ' + parent;
+        
+    if (/^[0-9]+$/.test(name)) {
+      return result + '[' + name + '])';
+    } else if (Handlebars.JavaScriptCompiler.isValidJavaScriptVariableName(name)) {
+      return result + '.' + name + ')';
+    } else {
+      return result + "['" + name + "'])";
+    }
+};
+
+module.exports = Handlebars

ddda4591d7ca9438c8a74028c08940c5f7fa8494
dan <dan@reactive.org>
2016-07-16 12:06:07 +1000

    Better impl. of nameLookup

--- runtime/index.js
+++ runtime/index.js
@@ -1,17 +1,10 @@
 // allow compile calls at runtime
 var Handlebars = require('handlebars'); 
 
-// support backbone model property lookups
+var originalNameLookup = Handlebars.JavaScriptCompiler.prototype.nameLookup;
+
 Handlebars.JavaScriptCompiler.prototype.nameLookup = function(parent, name, type) {
-    var result = '(' + parent + ' instanceof Backbone.Model ? ' + parent + '.get(\'' + name + '\') : ' + parent;
-        
-    if (/^[0-9]+$/.test(name)) {
-      return result + '[' + name + '])';
-    } else if (Handlebars.JavaScriptCompiler.isValidJavaScriptVariableName(name)) {
-      return result + '.' + name + ')';
-    } else {
-      return result + "['" + name + "'])";
-    }
+  return parent + ".get ? " + parent + ".get('" + name + "') : " + originalNameLookup.apply(this, arguments);
 };
 
 module.exports = Handlebars
